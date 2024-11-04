2a78c74f99347ce8fb26388de7d8df38dbb809eb
Maruiful <2023217418@mail.hfut.edu.cn>
2024-11-04 14:24:21 +0800

    feat：数字判空和零值判断工具类

--- paicoding-core/src/main/java/com/github/paicoding/forum/core/util/NumUtil.java
+++ paicoding-core/src/main/java/com/github/paicoding/forum/core/util/NumUtil.java
@@ -1,13 +1,23 @@
 package com.github.paicoding.forum.core.util;
 
-/** */
+/**
+ * 数字判空和零值判断工具类
+ */
 public class NumUtil {
 
-    public static boolean nullOrZero(Long num)  { return false; }
+    public static boolean nullOrZero(Long num) {
+        return num == null || num == 0L;
+    }
 
-    public static boolean nullOrZero(Integer num)  { return false; }
+    public static boolean nullOrZero(Integer num) {
+        return num == null || num == 0;
+    }
 
-    public static boolean upZero(Long num)  { return false; }
+    public static boolean upZero(Long num) {
+        return num != null && num > 0;
+    }
 
-    public static boolean upZero(Integer num)  { return false; }
+    public static boolean upZero(Integer num) {
+        return num != null && num > 0;
+    }
 }
