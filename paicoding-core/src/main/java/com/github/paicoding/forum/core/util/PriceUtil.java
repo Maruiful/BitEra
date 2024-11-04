5398998dbb1b9b004962066b820356da49726301
Maruiful <2023217418@mail.hfut.edu.cn>
2024-11-04 21:12:20 +0800

    feat：价格处理类

--- paicoding-core/src/main/java/com/github/paicoding/forum/core/util/PriceUtil.java
+++ paicoding-core/src/main/java/com/github/paicoding/forum/core/util/PriceUtil.java
@@ -6,7 +6,9 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
-/** */
+/**
+ * 价格处理类
+ */
 public class PriceUtil {
 
     /**
@@ -15,7 +17,14 @@ public class PriceUtil {
      * @param price
      * @return
      */
-    public static Integer toCentPrice(String price)  { return null; }
+    public static Integer toCentPrice(String price) {
+        if (StringUtils.isBlank(price)) {
+            return null;
+        }
+
+        BigDecimal ans = new BigDecimal(price).multiply(new BigDecimal("100.0")).setScale(0, RoundingMode.HALF_DOWN);
+        return ans.intValue();
+    }
 
     /**
      * 分的价格转元
@@ -23,7 +32,20 @@ public class PriceUtil {
      * @param price
      * @return
      */
-    public static String toYuanPrice(Integer price)  { return null; }
+    public static String toYuanPrice(Integer price) {
+        if (price == null) {
+            return null;
+        }
+        DecimalFormat df1 = new DecimalFormat("0.00");
+        String ans = df1.format(price / 100f);
+        if (price % 100 == 0) {
+            // 整元时，移除后面的小数
+            return ans.substring(0, ans.length() - 3);
+        } else if (price % 10 == 0) {
+            return ans.substring(0, ans.length() - 1);
+        }
+        return ans;
+    }
 
     /**
      * 判断是否为合法的价格
@@ -31,5 +53,12 @@ public class PriceUtil {
      * @param price
      * @return ture 合法价格
      */
-    public static boolean legalPrice(String price)  { return false; }
+    public static boolean legalPrice(String price) {
+        if (StringUtils.isBlank(price)) {
+            return false;
+        }
+
+        Integer pp = toCentPrice(price);
+        return pp != null && pp > 0;
+    }
 }
