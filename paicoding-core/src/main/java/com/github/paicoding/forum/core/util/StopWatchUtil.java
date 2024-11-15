96621d96567f688f0bff0354aac635f3655f2b10
Maruiful <2023217418@mail.hfut.edu.cn>
2024-11-15 21:36:51 +0800

    feat：同步计时器的实现

--- paicoding-core/src/main/java/com/github/paicoding/forum/core/util/StopWatchUtil.java
+++ paicoding-core/src/main/java/com/github/paicoding/forum/core/util/StopWatchUtil.java
@@ -6,7 +6,8 @@ import java.util.concurrent.Callable;
 
 /**
  * 统计耗时工具类, 这个只支持同步的耗时打印，不支持异步的场景
- * */
+ *
+ */
 public class StopWatchUtil {
     private StopWatch stopWatch;
 
@@ -20,7 +21,9 @@ public class StopWatchUtil {
      * @param task
      * @return
      */
-    public static StopWatchUtil init(String... task)  { return null; }
+    public static StopWatchUtil init(String... task) {
+        return new StopWatchUtil(task.length > 0 ? task[0] : null);
+    }
 
     /**
      * 同步耗时计时
@@ -47,7 +50,14 @@ public class StopWatchUtil {
      * @param task 任务名
      * @param run  执行业务逻辑
      */
-    public void record(String task, Runnable run)  {}
+    public void record(String task, Runnable run) {
+        stopWatch.start(task);
+        try {
+            run.run();
+        } finally {
+            stopWatch.stop();
+        }
+    }
 
 
     /**
@@ -55,5 +65,7 @@ public class StopWatchUtil {
      *
      * @return
      */
-    public String prettyPrint()  { return null; }
+    public String prettyPrint() {
+        return stopWatch.prettyPrint();
+    }
 }
