9b840d7aa26fc6c3c1ba72c16306c6bdb29d014f
Maruiful <2023217418@mail.hfut.edu.cn>
2025-04-13 09:51:58 +0800

    feat：error控制器的实现

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/error/CustomizeErrorController.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/error/CustomizeErrorController.java
@@ -15,7 +15,30 @@ import javax.servlet.http.HttpServletRequest;
 public class CustomizeErrorController implements ErrorController {
 
     @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView errorHtml(HttpServletRequest request, Model model)  { return null; }
+    public ModelAndView errorHtml(HttpServletRequest request, Model model) {
+        HttpStatus status = getStatus(request);
 
-    private HttpStatus getStatus(HttpServletRequest request)  { return null; }
+        if (status.is4xxClientError()) {
+            model.addAttribute("message", "你这个请求错了吧，要不要换个姿势！！！");
+
+        }
+        if (status.is5xxServerError()) {
+            model.addAttribute("message", "服务器冒烟了，要不然你稍后再试试！！！");
+        }
+
+        return new ModelAndView("error");
+
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        try {
+            return HttpStatus.valueOf(statusCode);
+        } catch (Exception ex) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+    }
 }
